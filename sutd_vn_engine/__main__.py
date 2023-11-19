b3fe1b7e6ba683e3d6c79658d50a994a4a77354c
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2023-11-19 13:17:46 +0800

    async function handling

--- sutd_vn_engine/__main__.py
+++ sutd_vn_engine/__main__.py
@@ -3,9 +3,13 @@
 import asyncio
 import base64
 import json
+import logging
 import tkinter as tk
+from calendar import c
 from tempfile import NamedTemporaryFile
 from urllib import request
+
+log = logging.getLogger(__name__)
 
 url = "http://robowsl.home.arpa:6000/sdapi/v1/txt2img"
 
@@ -22,13 +26,17 @@ def post_json(url, obj):
             "Content-Length": len(data),
         },
     )
-    return request.urlopen(req)
+    return asyncio.to_thread(request.urlopen, req)
 
 
-def get_image(prompt):
+async def get_image(prompt):
     """Get image from text."""
     obj = {"prompt": prompt}
-    res = post_json(url, obj)
+    try:
+        res = await post_json(url, obj)
+    except asyncio.CancelledError:
+        log.warning(f"cancelled: {prompt}")
+        return None
 
     data = json.loads(res.read())
     b64_img = data["images"][0]
@@ -43,22 +51,34 @@ async def main():
     app = tk.Tk()
     app.title("SUTD Visual Novel")
 
+    cur_req_task = None
+
     image_label = tk.Label(app)
     image_label.pack()
 
     prompt_input = tk.Text(app)
     prompt_input.pack()
 
-    def _update_image():
-        prompt = prompt_input.get("1.0", "end-1c")
-        print("prompt:", prompt)
-        path = get_image(prompt)
-        print("temp path:", path)
+    def _update_image(path):
+        log.info(f"temp path: {path}")
+        if path is None:
+            return
         image = tk.PhotoImage(file=path)
         image_label.image = image
         image_label.configure(image=image)
 
-    confirm = tk.Button(app, command=_update_image, text="Generate")
+    def _send_prompt():
+        nonlocal cur_req_task
+        if cur_req_task:
+            cur_req_task.cancel()
+            cur_req_task = None
+
+        prompt = prompt_input.get("1.0", "end-1c")
+        log.info(f"prompt: {prompt}")
+        cur_req_task = asyncio.create_task(get_image(prompt))
+        cur_req_task.add_done_callback(lambda f: _update_image(f.result()))
+
+    confirm = tk.Button(app, command=_send_prompt, text="Generate")
     confirm.pack()
 
     while True:
@@ -67,4 +87,5 @@ async def main():
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
     asyncio.run(main())
