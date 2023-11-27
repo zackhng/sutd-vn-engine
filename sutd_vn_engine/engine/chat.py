dc51949083ab6b273691f0e2057bc17ee8b74519
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2023-11-27 18:13:19 +0800

    make chat window dynamic size & test it

--- sutd_vn_engine/engine/chat.py
+++ sutd_vn_engine/engine/chat.py
@@ -1,41 +1,45 @@
 """Chat widget."""
 
 import asyncio
-from tkinter import *
-from tkinter.ttk import *  # type: ignore
+import tkinter as tk
+import tkinter.font as tkFont
+import tkinter.ttk as ttk
 
-from .utils import EM, LORUM
+from sutd_vn_engine.engine.utils import EM, LORUM
 
 __all__ = ["ChatLog"]
 
 
-class ChatLog(Labelframe):
+class ChatLog(ttk.Labelframe):
     """Chat Log GUI."""
 
-    def __init__(self, master=None, ncols=32, msgcols=22, colwidth=3 * EM):
+    def __init__(self, master=None, ncols=32, msgcols=22):
         """Constructor."""
         super(ChatLog, self).__init__(master=master, text="Chat Log")
         self.messages = []
         self.ncols = ncols
         self.msgcols = msgcols
-        self.colwidth = colwidth
 
-        self.set_speaker("", CENTER)
+        self.set_speaker("", "center")
         self._init_gui()
         self._init_style()
 
     def _init_gui(self):
         """Init GUI."""
-        canvas = Canvas(self, width=(self.ncols + 2) * self.colwidth)
-        inner = Frame(canvas)
-        scroll = Scrollbar(self, orient=VERTICAL, command=canvas.yview)
+        canvas = tk.Canvas(self, highlightthickness=0, bd=0)
+        inner = tk.Frame(canvas, bd=0)
+        scroll = ttk.Scrollbar(self, orient="vertical", command=canvas.yview)
 
         canvas.config(yscrollcommand=scroll.set)
-        canvas.create_window((canvas.winfo_reqwidth() / 2, 0), window=inner, anchor=N)
-        inner.columnconfigure(list(range(self.ncols)), minsize=self.colwidth, weight=0)
+        inner_id = canvas.create_window((0, 0), window=inner, anchor="n")
+        canvas.pack(fill="both", expand=True, side="left")
+        scroll.pack(fill="y", side="right")
 
-        canvas.pack(fill=BOTH, expand=True, side=LEFT)
-        scroll.pack(fill=Y, side=RIGHT)
+        def _on_canvas_configure(event):
+            canvas.coords(inner_id, event.width // 2, 0)
+            canvas.itemconfig(inner_id, width=event.width)
+
+        canvas.bind("<Configure>", _on_canvas_configure)
 
         self.canvas = canvas
         self.inner = inner
@@ -43,39 +47,31 @@ class ChatLog(Labelframe):
 
     def _init_style(self):
         """Init style."""
-        self.style = Style(self)
-        common = dict(
-            relief=RAISED,
-            wraplength=self.msgcols * self.colwidth,
-            padding=EM,
-        )
-        left = common | dict(background="white")
-        right = common | dict(background="lightgreen")
-        center = common | dict(background="lightblue", justify=CENTER)
-        self.style.configure("Left.TLabel", **left)
-        self.style.configure("Right.TLabel", **right)
-        self.style.configure("Center.TLabel", **center)
-
         self.placement = dict(
             columnspan=self.msgcols,
-            sticky=EW,
+            sticky="ew",
+            padx=EM,
             pady=(0.3 * EM, 0.7 * EM),
         )
+        self.style = ttk.Style(self)
+        common = dict(relief="raised", padding=EM)
 
-    def _calc_msg_height(self, msgdict):
-        """Calculate height of message."""
-        widget = msgdict["widget"]
-        padinfo = widget.grid_info()["pady"]
-        pad = padinfo if isinstance(padinfo, int) else sum(padinfo)
-        return widget.winfo_reqheight() + pad
+        def _on_inner_configure(event):
+            cwidth = event.width / self.ncols
+            common["wraplength"] = cwidth * self.msgcols - 4 * common["padding"]
+            self.inner.columnconfigure([*range(self.ncols)], minsize=cwidth, weight=1)
 
-    def _update_scroll(self):
-        """Update size of inner canvas for scrolling."""
-        height = 0
-        for m in self.messages:
-            height += m["height"]
-        self.canvas.config(scrollregion=(0, 0, 0, height))
-        self.canvas.yview_moveto(1)
+            left = common | dict(background="white")
+            right = common | dict(background="lightgreen")
+            center = common | dict(background="lightblue", justify="center")
+
+            self.style.configure("Left.TLabel", **left)
+            self.style.configure("Right.TLabel", **right)
+            self.style.configure("Center.TLabel", **center)
+
+            self.canvas.config(scrollregion=(0, 0, 0, event.height))
+
+        self.inner.bind("<Configure>", _on_inner_configure)
 
     def _msg(self, msg, name=None, side=None):
         """Common implementation for add_msg and add_anim_msg."""
@@ -84,33 +80,33 @@ class ChatLog(Labelframe):
         msg = f"{name}:\n{msg}" if name else msg
 
         row = len(self.messages)
-        anchor = CENTER if side == CENTER else W
-        if side == LEFT:
+        anchor = "center" if side == "center" else "w"
+        if side == "left":
             col = 0
             style = "Left.TLabel"
-        elif side == RIGHT:
+        elif side == "right":
             col = self.ncols - self.msgcols
             style = "Right.TLabel"
-        elif side == CENTER:
+        elif side == "center":
             col = (self.ncols - self.msgcols) // 2
             style = "Center.TLabel"
         else:
             raise ValueError(f"Side {side} not supported.")
 
-        textvar = StringVar(value=msg)
-        message = Label(self.inner, textvariable=textvar, style=style, anchor=anchor)
-        message.grid(**self.placement, row=row, column=col)
-        height = self._calc_msg_height(dict(widget=message))
-
-        self.messages.append(
-            dict(name=name, side=side, height=height, var=textvar, widget=message)
+        textvar = tk.StringVar(value=msg)
+        message = ttk.Label(
+            self.inner, textvariable=textvar, style=style, anchor=anchor
         )
-        self._update_scroll()
+        message.grid(**self.placement, row=row, column=col)
+
+        self.messages.append(dict(name=name, side=side, var=textvar, widget=message))
         return textvar, msg
 
     def add_msg(self, msg, name=None, side=None):
         """Add a message to the chat log."""
         self._msg(msg, name, side)
+        self.update_idletasks()
+        self.canvas.yview_moveto(1)
 
     async def add_anim_msg(self, msg, name=None, side=None, delay=0.01):
         """Add an message with typing animation to the chat log."""
@@ -122,10 +118,13 @@ class ChatLog(Labelframe):
                 cur += c
                 text.set(cur)
                 await asyncio.sleep(delay)
+                self.canvas.yview_moveto(1)
         except asyncio.CancelledError:
             pass
         finally:
             text.set(msg)
+            self.update_idletasks()
+            self.canvas.yview_moveto(1)
 
     def set_speaker(self, name=None, side=None):
         """Set the speaker name and side."""
@@ -136,25 +135,43 @@ class ChatLog(Labelframe):
 if __name__ == "__main__":
     from pprint import pprint
 
-    root = Tk()
+    root = tk.Tk()
     root.title("Chat Log Test")
-    root.configure(bg="pink")
+    default_font = tkFont.nametofont("TkDefaultFont")
+    default_font.config(family="Courier New", size=EM)
+    root.option_add("*Font", default_font)
+    root.config(bg="pink")
     root.geometry("1280x960")
     root.resizable(width=False, height=False)
 
     chatlog = ChatLog(root)
-    chatlog.pack(fill=Y, expand=True)
+    chatlog.pack(fill="both", expand=True)
 
-    chatlog.add_msg("This is the beginning of the conversation.")
+    pad = True
 
-    chatlog.set_speaker("You", RIGHT)
-    chatlog.add_msg("Hello, world! Why is this being split across two lines?")
-    chatlog.add_msg(LORUM)
+    def _resize(_):
+        global pad
+        if pad := not pad:
+            chatlog.pack(padx=20 * EM)
+        else:
+            chatlog.pack(padx=10 * EM)
 
-    chatlog.set_speaker("Someone Else", LEFT)
-    chatlog.add_msg(LORUM)
-    chatlog.add_msg(LORUM)
-    chatlog.add_msg(LORUM)
+    _resize(None)
+    chatlog.bind("<Button-1>", _resize)
 
-    pprint(chatlog.messages)
+    def _test():
+        chatlog.add_msg("This is the beginning of the conversation.")
+
+        chatlog.set_speaker("You", "right")
+        chatlog.add_msg("Hello, world! Why is this being split across two lines?")
+        chatlog.add_msg(LORUM)
+
+        chatlog.set_speaker("Someone Else", "left")
+        chatlog.add_msg(LORUM)
+        chatlog.add_msg(LORUM)
+        chatlog.add_msg(LORUM)
+
+        pprint(chatlog.messages)
+
+    root.after(500, _test)
     root.mainloop()
